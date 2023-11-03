8fd68b51f71dc58e1bb1612014a921ec0789bbfd
olaniyi-emmanuel <emmanuel4sucess@outlook.com>
2023-11-03 22:47:22 +0100

    Day 85: Around Advice Type

--- src/main/java/com/olaniyi/Aspect/LoggingAspect.java
+++ src/main/java/com/olaniyi/Aspect/LoggingAspect.java
@@ -1,6 +1,8 @@
 package com.olaniyi.Aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -22,6 +24,20 @@ public class LoggingAspect {
     @Before("args(name)")
     public void stringArgumentMethod(String name){
         System.out.println("A method that takes String arguments has been called. The value of the name is: " + name);
+    }
+    @Around("allGetters")
+    public  void myAroundAdvice(ProceedingJoinPoint proceedingJoinPoint){
+
+        try {
+            System.out.println("Before Advice");
+            proceedingJoinPoint.proceed();
+            System.out.println("after returning ");
+        } catch (Throwable e) {
+            System.out.println("Before throwing execptions/error");
+            throw new RuntimeException(e);
+        }
+        System.out.println("after finally");
+
     }
 
     @Pointcut("execution(* get*())")
